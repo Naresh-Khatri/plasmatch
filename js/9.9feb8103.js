c321b9a9a7a667adbea14c8b063e7a13bf83c9c6
Naresh <naresh.khatri2345@gmail.com>
2021-05-01 00:54:33 +0530

    fixed image responsiveness

---
+++ js/9.9feb8103.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[9],{bd8c:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},l=[],u={},r=u,s=t("2877"),a=Object(s["a"])(r,c,l,!1,null,null,null);e["default"]=a.exports}}]);
